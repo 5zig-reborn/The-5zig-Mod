a3c9c05e697d57fb1f3ab24119262f9dbaee91af
RoccoDev <roccodev.business@gmail.com>
2019-06-19 00:28:56 +0200

    Fix working directory

--- .travis/git.sh
+++ .travis/git.sh
@@ -10,7 +10,7 @@ fi
 git config --local user.name "Travis CI"
 git config --local user.email "travis@5zigreborn.eu"
 
-rsync -av ../../version-specific/artifacts/ .
+rsync -av ../version-specific/artifacts/ .
 git add --all
 git commit -m "${TRAVIS_COMMIT_MESSAGE} (${TRAVIS_COMMIT})"
 git push "https://${GITHUB_TOKEN}@github.com/5zig-reborn/deployments.git" master
