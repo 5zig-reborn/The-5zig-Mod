6168d1bcb0eb5e43fa9aa8877b2d5d6fed2c9173
RoccoDev <roccodev.business@gmail.com>
2019-06-19 00:10:06 +0200

    Auto-deployment test

---
+++ .travis/git.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+git clone --depth 1 https://github.com/5zig-reborn/deployments
+cd deployments
+
+if [! $TRAVIS_BRANCH = "master" ]; then
+    exit
+fi
+
+git config --local user.name "Travis CI"
+git config --local user.email "travis@5zigreborn.eu"
+
+mv ../../version-specific/artifacts/* .
+git add --all
+git commit -m "${TRAVIS_COMMIT_MESSAGE} (${TRAVIS_COMMIT})"
+git push "https://${GITHUB_TOKEN}@github.com/5zig-reborn/deployments.git" master
