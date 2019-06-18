50b077a52f40c994b97972426892382909e823de
RoccoDev <roccodev.business@gmail.com>
2019-06-19 00:20:33 +0200

    Fix spaces

--- .travis/git.sh
+++ .travis/git.sh
@@ -3,14 +3,14 @@
 git clone --depth 1 https://github.com/5zig-reborn/deployments
 cd deployments
 
-if [! $TRAVIS_BRANCH = "master" ]; then
+if [ ! $TRAVIS_BRANCH = "master" ]; then
     exit
 fi
 
 git config --local user.name "Travis CI"
 git config --local user.email "travis@5zigreborn.eu"
 
-mv ../../version-specific/artifacts/* .
+rsync -av ../../version-specific/artifacts/ .
 git add --all
 git commit -m "${TRAVIS_COMMIT_MESSAGE} (${TRAVIS_COMMIT})"
 git push "https://${GITHUB_TOKEN}@github.com/5zig-reborn/deployments.git" master
