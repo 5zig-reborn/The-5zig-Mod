8357d1d63bc57dc5abf8d2d38dd586860bd2eac3
RoccoDev <roccodev.business@gmail.com>
2019-07-06 13:34:52 +0200

    [Travis test] Build might succeed with just one wrapper

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-# Build for versions that need the 4.9 wrapper
-./gradlew :1.13.2:build
-
-# Install the new wrapper
-./gradlew wrapper --gradle-version 4.4.1
-
 # Build
-./gradlew -c .travis/1_8_9.gradle :1.8.9:build
-./gradlew -c .travis/1_12_2.gradle :1.12.2:build
+./gradlew build
 
 # Move artifacts
 cd version-specific
