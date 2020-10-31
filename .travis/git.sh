58c75fc8345f115444388178932f648e705eb800
RoccoDev <hey@rocco.dev>
2020-10-31 19:30:09 +0100

    ci: multiple deployment branches

--- .travis/git.sh
+++ .travis/git.sh
@@ -2,10 +2,7 @@
 GIT_REV=$(git rev-list --count master)
 git clone --depth 1 https://github.com/5zig-reborn/deployments
 cd deployments
-
-if [ ! $TRAVIS_BRANCH = "master" ]; then
-    exit
-fi
+git checkout -b $TRAVIS_BRANCH
 
 git config --local user.name "Travis CI"
 git config --local user.email "travis@5zigreborn.eu"
@@ -14,4 +11,4 @@ rm *
 rsync -av ../version-specific/artifacts/ .
 git add --all
 git commit -m "${TRAVIS_COMMIT_MESSAGE} (${TRAVIS_COMMIT})" -m "Git-Rev: [${GIT_REV}]"
-git push "https://${GITHUB_TOKEN}@github.com/5zig-reborn/deployments.git" master
+git push "https://${GITHUB_TOKEN}@github.com/5zig-reborn/deployments.git"
