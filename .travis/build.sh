eada2319e62fc298470f3e4fd254ede46221446d
RoccoDev <hey@rocco.dev>
2020-12-07 22:03:46 +0100

    build: Fabric patcher mods dir

--- .travis/build.sh
+++ .travis/build.sh
@@ -9,10 +9,13 @@ set -e
 cd version-specific
 # Build 1.15.x fabric jar
 cd 1.15.1/build/libs
+mkdir mods
+cp *.jar mods
 wget https://launcher.mojang.com/v1/objects/e3f78cd16f9eb9a52307ed96ebec64241cc5b32d/client.jar
 wget -O fabric.jar https://github.com/5zig-reborn/5zig-fabric/releases/download/2.0.1/5zig-Fabric-Installer-2.0.1.jar
 java -jar fabric.jar client.jar
 rm client.jar fabric.jar # Delete MC client jar and remapper
+rm -r mods
 cd -
 mkdir artifacts
 mv **/build/libs/*.jar artifacts
