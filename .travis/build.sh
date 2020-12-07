7282763af2b15f82c714955f101dc4580b2db147
RoccoDev <hey@rocco.dev>
2020-12-07 21:58:06 +0100

    build: Fabric patcher 2.0.1

--- .travis/build.sh
+++ .travis/build.sh
@@ -10,7 +10,7 @@ cd version-specific
 # Build 1.15.x fabric jar
 cd 1.15.1/build/libs
 wget https://launcher.mojang.com/v1/objects/e3f78cd16f9eb9a52307ed96ebec64241cc5b32d/client.jar
-wget -O fabric.jar https://github.com/5zig-reborn/5zig-fabric/releases/download/2.0/5zig-Fabric-Installer-2.0.jar
+wget -O fabric.jar https://github.com/5zig-reborn/5zig-fabric/releases/download/2.0.1/5zig-Fabric-Installer-2.0.1.jar
 java -jar fabric.jar client.jar
 rm client.jar fabric.jar # Delete MC client jar and remapper
 cd -
